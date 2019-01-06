08fc5f015f116487690aa7564d9ec209191098d5
Kim T <kimturley@gmail.com>
2019-01-06 18:22:51 -0500

    Angular Service Worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 export function init(routeService: AppRoutingService) {
   console.log('init');
@@ -33,7 +35,8 @@ export function init(routeService: AppRoutingService) {
     ReactiveFormsModule,
     TransferHttpCacheModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   exports: [
     // DragScrollModule
