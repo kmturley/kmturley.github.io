d5b4a352318304ff9a5540c9cc5cb400acebe609
Kim T <kimturley@gmail.com>
2019-01-10 22:57:58 -0500

    Separate function for parsing Google Sheets data

--- static.paths.ts
+++ static.paths.ts
@@ -2,7 +2,7 @@ import { environment } from './src/environments/environment.prod';
 
 const request = require('request');
 const slugify = require('slugify');
-const routes = ['/'];
+const routes = ['/', '/projects'];
 const req = request.defaults({
   headers: {
     'Authorization': `Bearer ${environment.token}`
@@ -11,19 +11,54 @@ const req = request.defaults({
 
 export function getPaths() {
   return new Promise((resolve, reject) => {
+    const id = 'projects';
     console.log(`getPaths: ${environment.API_URL}${environment.SHEET_ID}?includeGridData=true`);
-    req.get(`${environment.API_URL}${environment.SHEET_ID}?includeGridData=true`, (err, res, data) => {
+    req.get(`${environment.API_URL}${environment.SHEET_ID}?includeGridData=true`, (err, res, items) => {
       console.log('getPaths complete');
       if (err) { return reject(err); }
-      data = JSON.parse(data);
-      if (data['error']) { return reject(data); }
-      const rows = data['sheets'][0]['data'][0]['rowData'];
-      rows.forEach((rowItem, rowIndex) => {
-        if (rowIndex > 0 && rowItem['values'][0].formattedValue) {
-          routes.push(slugify(rowItem['values'][0].formattedValue, { lower: true }));
-        }
+      items = JSON.parse(items);
+      if (items['error']) { return reject(items); }
+      if (items['sheets']) {
+        const sheetItems = convertSheets(items['sheets']);
+        Object.keys(sheetItems).forEach((sheetId) => {
+          if (sheetId === id) {
+            items = sheetItems[sheetId];
+          }
+        });
+      }
+      items.forEach((item) => {
+        routes.push('/' + item.path);
       });
       resolve(routes);
     });
   });
+}
+
+function convertSheets(sheets: Array<object>) {
+  const sheetList = {};
+  sheets.forEach((sheet) => {
+    const rowItems = [];
+    const rows = sheet['data'][0]['rowData'];
+    rows.forEach((row, index) => {
+      if (index > 0) {
+        const newRow = {};
+        row['values'].forEach((rowItem, rowIndex) => {
+          const rowKey = rows[0]['values'][rowIndex].formattedValue;
+          let rowValue = rowItem.formattedValue;
+          if (rowKey && rowValue) {
+            if (rowKey.charAt(rowKey.length - 1) === 's') {
+              rowValue = rowItem.formattedValue.split('\n');
+            }
+            newRow[rowKey] = rowValue;
+          }
+        });
+        if (newRow['name']) {
+          newRow['path'] = slugify(sheet['properties']['title'], { lower: true }) + '/' + slugify(newRow['name'], { lower: true });
+          rowItems.push(newRow);
+        }
+      }
+    });
+    sheetList[sheet['properties']['title']] = rowItems;
+  });
+  return sheetList;
 }
